0cda7a6d806c37fe79d7d82183ba3b6c3f8217b0
sushilthapa <sushil@spark.com.np>
2017-03-16 17:24:13 +0545

    added error forbidden and internal error codes custom pages

--- app/__init__.py
+++ app/__init__.py
@@ -45,4 +45,16 @@ def create_app(config_name):
     from .home import home as home_blueprint
     app.register_blueprint(home_blueprint)
 
+    @app.errorhandler(403)
+    def forbidden(error):
+        return render_template('errors/403.html', title='Forbidden'), 403
+
+    @app.errorhandler(404)
+    def page_not_found(error):
+        return render_template('errors/404.html', title='Page Not Found'), 404
+
+    @app.errorhandler(500)
+    def internal_server_error(error):
+        return render_template('errors/500.html', title='Server Error'), 500
+
     return app
